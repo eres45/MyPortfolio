8cc07011ede041b63239b26b92c0252caf6ac8f7
eres45 <131444289+eres45@users.noreply.github.com>
2025-01-07 19:53:21 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,31 +2,21 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src')
-    }
-  },
+  root: process.cwd(),
+  base: '/',
   build: {
     outDir: 'dist',
     sourcemap: true,
     rollupOptions: {
-      input: {
-        app: path.resolve(__dirname, 'index.html')
-      },
-      external: [
-        'react',
-        'react-dom',
-        'react-dom/client'
-      ]
+      input: path.resolve(__dirname, 'index.html')
     }
   },
-  optimizeDeps: {
-    include: ['react', 'react-dom']
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
   },
   server: {
     port: 3000,
